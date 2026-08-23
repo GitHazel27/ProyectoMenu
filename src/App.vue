5bc1b7fd174ee3eb10df30c5dd3784b0f2323dd8
GitHazel27 <hazelsilva270408@gmail.com>
2026-08-23 14:59:05 -0600

    subida de archivos

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue';
+
+const  myId="coffe"
+let myCondition=false
+let condition2=false
+const movies=["Lord of the Rings", "Star Wars", "Dune", "Batman"]
+</script>
+
+<template>
+  <HeaderComponent></HeaderComponent>
+  <h1 class="title" :class="myCondition ? 'highlight':'red'">Hola a todos</h1>
+  <HelloWorld />
+</template>
+
+<style scoped>
+
+.title{
+  color:blueviolet
+}
+
+.highlight{
+  color:antiquewhite
+}
+
+.red{
+  color:red
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <nav>
+           <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+           </ul> 
+        </nav>
+    </header>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scope>
+</style>
